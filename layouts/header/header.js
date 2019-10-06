efcb33ad03b2d23c5ac35f48fc48d35eb2987e10
Mark Riggan <mark.riggan@atlanticbt.com>
2019-10-06 07:15:15 -0400

    Added navigation

--- layouts/header/header.js
+++ layouts/header/header.js
@@ -1,10 +1,24 @@
+// Import nodes
 import React from "react"
+import Link from 'next/link'
+
+// Import styles
 // import './footer.scss'
 
 const Header = props => {
   return (
     <div>
       <h1>Mark Riggan</h1>
+
+      <nav>
+        <ul>
+          <li><Link href="/"><a>Home</a></Link></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">What I Do</a></li>
+          <li><Link href="/about"><a>About</a></Link></li>
+          <li><Link href="/contact"><a>Contact</a></Link></li>
+        </ul>
+      </nav>
     </div>
   )
 }
