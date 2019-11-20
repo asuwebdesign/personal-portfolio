769120d2e53bcc3252e2dbba9bbcf606b1fd7a7f
Mark Riggan <mark.riggan@atlanticbt.com>
2019-11-20 16:15:20 -0500

    Fix config

--- next.config.js
+++ next.config.js
@@ -14,26 +14,28 @@ const nextConfig = compose([
       placeholderSize: 50
     }
   }],
-  [workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
+  {
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'https-calls',
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
           },
         },
-      },
-    ],
-  }],
+      ],
+    },
+  },
 ])
 
 module.exports = withOffline(nextConfig)
