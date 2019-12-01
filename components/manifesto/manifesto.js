86d9645cc5c5bb3e55a94e4dc7afd30076ed0b6e
Mark Riggan <mark.riggan@atlanticbt.com>
2019-12-01 09:32:36 -0500

    New styles for manifesto

--- components/manifesto/manifesto.js
+++ components/manifesto/manifesto.js
@@ -12,35 +12,34 @@ const Manifesto = props => {
         <Wrapper>
           <div className="values__primary">
             <div className="values__number" aria-hidden="true">01</div>
-            <h2 className="values__title">How we act.</h2>
+            <h1 className="values__title">My core values</h1>
           </div>
           <div className="values__secondary">
-            <div>Sharing is Caring</div>
-            <div>Work together.</div>
+            <h2>Sharing is Caring. Work together.</h2>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
-            <ul>
-              <li>
-                <h3>Creativity</h3>
+            <ul className="values__list">
+              <li className="values__listitem">
+                <h2>Creativity</h2>
                 <p>Embrace a diverse set of perspectives and use your imagination to drive innovation.</p>
               </li>
-              <li>
-                <h3>Empathy</h3>
+              <li className="values__listitem">
+                <h2>Empathy</h2>
                 <p>Infuse your actions and work with respect for the emotions and experiences of others.</p>
               </li>
-              <li>
-                <h3>Integrity</h3>
+              <li className="values__listitem">
+                <h2>Integrity</h2>
                 <p>Consistently demonstrate dignity, honesty, transparency, and accountability.</p>
               </li>
-              <li>
-                <h3>Passion</h3>
+              <li className="values__listitem">
+                <h2>Passion</h2>
                 <p>Feel the power and love that comes from focusing on what excites you.</p>
               </li>
-              <li>
-                <h3>Quality</h3>
+              <li className="values__listitem">
+                <h2>Quality</h2>
                 <p>Always seek to produce excellent work that pushes the status quo forward.</p>
               </li>
-              <li>
-                <h3>Simplicity</h3>
+              <li className="values__listitem">
+                <h2>Simplicity</h2>
                 <p>Do more with less by striving for clarity, consistency, and focusing on what’s essential.</p>
               </li>
             </ul>
@@ -52,23 +51,22 @@ const Manifesto = props => {
         <Wrapper>
           <div className="philosophy__primary">
             <div className="philosophy__number" aria-hidden="true">02</div>
-            <h2 className="philosophy__title">How we think</h2>
+            <h1 className="philosophy__title">My philosophy</h1>
           </div>
           <div className="philosophy__secondary">
-            <div>Design makes everything possible</div>
-            <div>Design philosophy</div>
+            <h2>Design makes everything possible</h2>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
-            <ol>
-              <li>Simplicity wins.</li>
-              <li>Authenticity is everything.</li>
-              <li>Strategy is the brother of creativity.</li>
-              <li>Credibility begins with aesthetics.</li>
-              <li>A focus on culture moves a brand forward.</li>
-              <li>The consumer rules our world.</li>
-              <li>Don’t lose emotion in execution.</li>
-              <li>Design isn’t art – art is a servant to design.</li>
-              <li>Technology is creative.</li>
-              <li>Results matter.</li>
+            <ol className="philosophy__list">
+              <li className="philosophy__listitem">Simplicity wins.</li>
+              <li className="philosophy__listitem">Authenticity is everything.</li>
+              <li className="philosophy__listitem">Strategy is the brother of creativity.</li>
+              <li className="philosophy__listitem">Credibility begins with aesthetics.</li>
+              <li className="philosophy__listitem">A focus on culture moves a brand forward.</li>
+              <li className="philosophy__listitem">The consumer rules our world.</li>
+              <li className="philosophy__listitem">Don’t lose emotion in execution.</li>
+              <li className="philosophy__listitem">Design isn’t art – art is a servant to design.</li>
+              <li className="philosophy__listitem">Technology is creative.</li>
+              <li className="philosophy__listitem">Results matter.</li>
             </ol>
           </div>
         </Wrapper>
