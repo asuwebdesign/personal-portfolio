c22e6f105d3a0509fb5ca3a18b8ef484fbf120a3
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-06-03 14:04:21 -0400

    Added images to open telco

--- pages/projects/open-telco.js
+++ pages/projects/open-telco.js
@@ -85,6 +85,42 @@ const ProjectDetail = props => (
             <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
           </div>
         </Band>
+        <Band className="band--numbered">
+          <div className="band__title-block">
+            <div className="band__number">03</div>
+            <h1 className="band__title">Approach</h1>
+          </div>
+          <div className="band__content">
+            <h2>Designing the future.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+            <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
+            <Image src="/images/projects/open-telco/opentelco--5g-core--disconnected-environment.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--5g-core--key-principles.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--5g-core--networking.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--5g-core--scalability.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--5g-core--service-assurance--openstack.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--5g-core--service-assurance.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--closed-captioning--enabled.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--cover.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--dashboard--preloading.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--dashboard.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ran--open-ran--5g-call.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ran--open-ran--default.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ran--open-ran--intel-flexran.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--sustainability--intel-flexran--5g-ran-profile.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--sustainability--intel-flexran--building-blocks.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--sustainability--intel-flexran--kepler.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--compute.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--default.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--default3.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--floor-plan.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--partners.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--telco-solutions-lab--world-map.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ztp--5g-ran--profile.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ztp--default.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/open-telco/opentelco--ztp--tech-stack.png" alt="" width={1920} height={1080} />
+          </div>
+        </Band>
       </article>
     </main>
     <Detour meta="Next Case Study" title="Hwy 55" link="/projects/hwy55" />
