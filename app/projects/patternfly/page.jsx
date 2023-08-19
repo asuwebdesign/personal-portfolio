489b2e97dd5022e93d72cd71896042f7889a1ce8
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-19 15:54:31 -0400

    Updated covers

--- app/projects/patternfly/page.jsx
+++ app/projects/patternfly/page.jsx
@@ -27,15 +27,6 @@ export default function Page() {
                 <h2>About PatternFly</h2>
                 <p>PatternFly is an open-source design system and UI framework that simplifies and accelerates the creation of consistent and user-friendly web applications. Developed collaboratively by Red Hat and the open-source community, PatternFly provides a set of design patterns, components, and guidelines that ensure a cohesive visual and interactive experience across applications. It empowers designers and developers to efficiently build responsive and accessible interfaces, fostering a seamless user experience while saving time and effort in the development process.</p>
               </div>
-              {/* <div className="band__skills">
-                <h2>Skills provided</h2>
-                <ul className="tags">
-                  <li className="tag">Branding Strategy &amp; Identity</li>
-                  <li className="tag">Creative &amp; Experience Design</li>
-                  <li className="tag">Research, Data &amp; Analytics</li>
-                  <li className="tag">Websites &amp; Digital Platforms</li>
-                </ul>
-              </div> */}
             </div>
           </Band>
           <Band className="band--numbered">
@@ -60,39 +51,37 @@ export default function Page() {
               <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
             </div>
           </Band>
-          <Band className="band--numbered">
-            <div className="band__title-block">
-              <div className="band__number">03</div>
-              <h1 className="band__title">Approach</h1>
-            </div>
-            <div className="band__content">
-              <h2>Designing the future.</h2>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-              <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.</p>
-              <Image src="/images/projects/patternfly/pf5--hcc--ansible--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--ansible--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--app-services--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--console--app-services--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--console--rhel--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--home--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--home--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--openshift--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--openshift--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--rhel--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--signin--username--dark.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--hcc--signin--username--light.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--builds.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--dashboard.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--installed.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--nav-panel.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--networking.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--observe.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--operators.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--signin.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--storage.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp--workloads.png" alt="" width={1920} height={1080} />
-              <Image src="/images/projects/patternfly/pf5--ocp-compute.png" alt="" width={1920} height={1080} />
-            </div>
+          <Band div>
+            <Image src="/images/projects/patternfly/pf5--ocp--dashboard.png" alt="" width={1920} height={1080} />
+          </Band>
+          <Band className="band--split" div>
+            <Image src="/images/projects/patternfly/pf5--ocp--signin.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--installed.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--networking.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--observe.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--operators.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--storage.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--workloads.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp-compute.png" alt="" width={1920} height={1080} />
+          </Band>
+          <Band div>
+            <Image src="/images/projects/patternfly/pf5--hcc--home--dark.png" alt="" width={1920} height={1080} />
+          </Band>
+          <Band className="band--split" div>
+            <Image src="/images/projects/patternfly/pf5--hcc--signin--username--dark.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--ansible--dark.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--console--app-services--dark.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--openshift--dark.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--rhel--dark.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--ocp--builds.png" alt="" width={1920} height={1080} />
+          </Band>
+          <Band className="band--split" div>
+            <Image src="/images/projects/patternfly/pf5--hcc--signin--username--light.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--ansible--light.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--app-services--light.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--console--rhel--light.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--home--light.png" alt="" width={1920} height={1080} />
+            <Image src="/images/projects/patternfly/pf5--hcc--openshift--light.png" alt="" width={1920} height={1080} />
           </Band>
         </article>
       </main>
