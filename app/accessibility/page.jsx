7a68a1d6362f1e3cfcd4b9ef4b92f32ba4f1e588
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-01-01 15:43:25 -0500

    Legal updates

--- app/accessibility/page.jsx
+++ app/accessibility/page.jsx
@@ -1,6 +1,8 @@
 import Sidekick from "@/components/sidekick/sidekick"
 import Band from "@/layouts/band/band"
 import Detour from '@/components/detour/detour'
+import Poster from "@/components/poster/poster"
+import PosterSrc from '@/public/images/banners/banner-accessibility.jpg'
 
 export const metadata = {
   title: 'Accessibility - Mark Riggan',
@@ -13,11 +15,13 @@ export default function Page() {
       <main>
         <article>
           <Sidekick meta="Accessibility" title="Open commitment to accessibility." section />
-          <Band>
+          <Poster src={PosterSrc} alt="Hands with mobile, double exposure." />
+          <Band className="band--legal">
             <h3>Last Updated: August 18, 2023</h3>
             <p>At Mark Riggan's personal portfolio website, we are committed to ensuring that our website is accessible to everyone, regardless of disabilities or impairments. We strive to provide a user-friendly experience for all visitors, including those with visual, auditory, motor, and cognitive impairments. We believe in inclusive design and are continuously working to improve the accessibility of our website.</p>
-            
-            <h3>Our Efforts Toward Accessibility:</h3>
+          </Band>
+          <Band className="band--legal">
+            <h3>Our efforts toward accessibility</h3>
             <ul>
               <li>Alternative Text: We aim to provide alternative text descriptions for all images on the website, making visual content understandable to screen readers.</li>
               <li>Keyboard Navigation: Our website is designed to be navigable using a keyboard alone, ensuring that individuals who cannot use a mouse can access all content.</li>
@@ -25,18 +29,22 @@ export default function Page() {
               <li>Responsive Design: Our website is built with a responsive design that adapts to various screen sizes, ensuring content remains accessible on different devices.</li>
               <li>Captions and Transcripts: Whenever video or audio content is presented, we strive to provide captions and transcripts to make the content accessible to individuals with hearing impairments.</li>
             </ul>
-            
-            <h3>Ongoing Accessibility Improvements:</h3>
+          </Band>
+          <Band className="band--legal">
+            <h3>Ongoing accessibility improvements</h3>
             <p>We are actively working to enhance the accessibility of our website, including regular accessibility audits and user testing to identify and address potential issues. We aim to adhere to the Web Content Accessibility Guidelines (WCAG) 2.1 Level AA standards to make our website more accessible.</p>
-            
-            <h3>Feedback and Assistance:</h3>
+          </Band>
+          <Band className="band--legal">
+            <h3>Feedback and assistance</h3>
             <p>If you encounter any accessibility barriers while using our website or have suggestions for improvement, please contact us at [your contact information]. Your feedback is invaluable to us as we continue to enhance the accessibility of our website.</p>
             <p><strong>Third-Party Content:</strong> While we strive to ensure that our content meets accessibility standards, some third-party content or components may not be fully accessible. We encourage you to provide feedback on any accessibility issues you encounter with these elements.</p>
-            
-            <h3>External Accessibility Resources:</h3>
+          </Band>
+          <Band className="band--legal">
+            <h3>External accessibility resources</h3>
             <p>If you're seeking additional information or resources to improve your browsing experience, various browser extensions and assistive technologies are available that can help adjust settings based on your needs.</p>
-            
-            <h3>Future Commitment:</h3>
+          </Band>
+          <Band className="band--legal">
+            <h3>Future commitment</h3>
             <p>We are committed to maintaining and improving the accessibility of our website. As technology evolves and accessibility standards progress, we will continue to enhance our website to provide an inclusive experience for all users.</p>
             <p>By visiting our website, you acknowledge our commitment to accessibility and our ongoing efforts to improve it. We welcome your input to help us achieve this goal and create a positive experience for every visitor.</p>
             <p>If you have any questions or suggestions regarding the accessibility of our website, please reach out to us at [your contact information].</p>
--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -1,6 +1,9 @@
 import Band from "@/layouts/band/band"
 import Detour from '@/components/detour/detour'
-import Sidekick from "@/components/sidekick/sidekick"
+import Sidekick from '@/components/sidekick/sidekick'
+import Link from 'next/link'
+import Poster from "@/components/poster/poster"
+import PosterSrc from '@/public/images/banners/banner-terms.jpg'
 
 export const metadata = {
   title: 'Terms of Service - Mark Riggan',
@@ -13,37 +16,45 @@ export default function Page() {
       <main>
         <article>
           <Sidekick meta="Terms &amp; Conditions" title="Know your usage rights and restrictions." section />
-          <Band>
-            <p>These terms and conditions ("Terms") govern your use of Mark Riggan's Personal Portfolio Website (referred to as the "Website"). By accessing and using the Website, you agree to abide by these Terms. If you do not agree with these Terms, please refrain from using the Website.</p>
-
-            <h3>Use of Content:</h3>
-            <p>The content on the Website, including but not limited to text, images, videos, graphics, and design elements, is protected by copyright and other intellectual property laws. You may not reproduce, distribute, modify, or create derivative works based on the content without the explicit permission of Mark Riggan.</p>
-
-            <h3>Privacy:</h3>
-            <p>Your use of the Website is subject to the Privacy Policy, which outlines how personal data and information are collected, stored, and used. Please review the Privacy Policy to understand how your data is handled.</p>
-
-            <h3>User Conduct:</h3>
-            <p>You agree not to use the Website for any purpose that is unlawful, unethical, or infringes upon the rights of others. This includes but is not limited to transmitting any harmful or malicious content, engaging in unauthorized access or data mining, or interfering with the Website's functionality.</p>
-
-            <h3>Third-Party Links:</h3>
-            <p>The Website may contain links to third-party websites or resources that are not owned or controlled by [Your Name]. These links are provided for your convenience, and [Your Name] is not responsible for the content, functionality, or security of these external sites. Your use of third-party websites is subject to their respective terms and policies.</p>
-
-            <h3>Disclaimer of Warranties:</h3>
-            <p>The Website is provided "as is" and [Your Name] makes no warranties, express or implied, regarding its accuracy, reliability, or availability. [Your Name] disclaims all liability for any errors or omissions in the content of the Website and any damages that may arise from your use of the Website.</p>
-
-            <h3>Limitation of Liability:</h3>
-            <p>In no event shall [Your Name] be liable for any direct, indirect, incidental, consequential, special, or punitive damages arising out of or in any way connected with your use or inability to use the Website, even if [Your Name] has been advised of the possibility of such damages.</p>
-
-            <h3>Changes to Terms and Website:</h3>
-            <p>[Your Name] reserves the right to modify or update these Terms at any time. It is your responsibility to review these Terms periodically. [Your Name] also reserves the right to modify, suspend, or discontinue any aspect of the Website without notice.</p>
-
-            <h3>Governing Law:</h3>
-            <p>These Terms shall be governed by and construed in accordance with the laws of [Your Jurisdiction], without regard to its conflict of law principles.</p>
-
-            <h3>Contact:</h3>
-            <p>If you have any questions or concerns about these Terms or the Website, please contact [Your Contact Information].</p>
-
-            <p>By using the Personal Portfolio Website, you acknowledge that you have read, understood, and agreed to these Terms and Conditions.</p>
+          <Poster src={PosterSrc} alt="Person using MacBook Pro." />
+          <Band className="band--legal">
+            <p>These terms and conditions ("terms") govern your use of this website. By accessing and using the website, you agree to abide by these terms. If you do not agree with these terms, please refrain from using the website.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Use of content</h3>
+            <p>The content on the website, including but not limited to text, images, videos, graphics, and design elements, is protected by copyright and other intellectual property laws. You may not reproduce, distribute, modify, or create derivative works based on the content without the explicit permission of Mark Riggan.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Privacy</h3>
+            <p>Your use of the website is subject to the privacy policy, which outlines how personal data and information are collected, stored, and used. Please review the privacy policy to understand how your data is handled.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>User conduct</h3>
+            <p>You agree not to use the website for any purpose that is unlawful, unethical, or infringes upon the rights of others. This includes but is not limited to transmitting any harmful or malicious content, engaging in unauthorized access or data mining, or interfering with the Website's functionality.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Third-party links</h3>
+            <p>The website may contain links to third-party websites or resources that are not owned or controlled by us. These links are provided for your convenience, and we are not responsible for the content, functionality, or security of these external sites. Your use of third-party websites is subject to their respective terms and policies.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Disclaimer of warranties</h3>
+            <p>The website is provided "as is" and we make no warranties, express or implied, regarding its accuracy, reliability, or availability. We disclaim all liability for any errors or omissions in the content of the Website and any damages that may arise from your use of the Website.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Limitation of liability</h3>
+            <p>In no event shall we be liable for any direct, indirect, incidental, consequential, special, or punitive damages arising out of or in any way connected with your use or inability to use the Website, even if we have been advised of the possibility of such damages.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Changes to terms and website</h3>
+            <p>We reserve the right to modify or update these terms at any time. It is your responsibility to review these terms periodically. We also reserve the right to modify, suspend, or discontinue any aspect of the website without notice.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Governing law</h3>
+            <p>These terms shall be governed by and construed in accordance with the laws of North Carolina, without regard to its conflict of law principles.</p>
+          </Band>
+          <Band className="band--legal">
+            <h3>Contact</h3>
+            <p>If you have any questions or concerns about these terms or the website, please <Link href="/contact">contact us</Link>. By using the website, you acknowledge that you have read, understood, and agreed to these terms and conditions.</p>
           </Band>
         </article>
       </main>
